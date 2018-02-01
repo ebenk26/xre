9ca31ba7554cc11076582a836743cdcc22cf92ae
Kenji Azimi <kenji.azimi@gmail.com>
2018-02-01 18:34:06 +0700

    Show Currency Name on Job Post Page

--- application/models/employer_model.php
+++ application/models/employer_model.php
@@ -107,11 +107,12 @@ class Employer_Model extends CI_Model{
     }
 
     function get_job_detail($id){
-        $this->db->select('job_positions.*, position_levels.name as position_name, employment_types.name as employment_name, profile_uploads.name as img');
+        $this->db->select('job_positions.*, position_levels.name as position_name, employment_types.name as employment_name, profile_uploads.name as img, forex.name as forex_name');
         $this->db->from('job_positions');
         $this->db->join('position_levels', 'job_positions.position_level_id = position_levels.id', 'left');
         $this->db->join('employment_types', 'job_positions.employment_type_id = employment_types.id', 'left');
         $this->db->join('profile_uploads', 'job_positions.user_id = profile_uploads.user_id', 'left');
+		$this->db->join('forex', 'job_positions.forex = forex.id', 'left');
         $this->db->where('job_positions.id', $id);
         $query = $this->db->get();
         return $query->last_row();
