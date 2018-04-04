f045cea0bc80ea389c94687fe305d6833e1f07b2
Dea Rico Khasandra <dearicokhasandra@gmail.com>
2018-04-04 16:17:52 +0700

    update img croppic

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -32,32 +32,46 @@
 
       gtag('config', 'UA-115543574-1');
     </script>
+
+    <style type="text/css">
+    .animation{
+        visibility: visible; 
+        animation-delay: 0.1s; 
+        animation-name: fadeInUp;
+    }
+    </style>
     
 </head>
 
 <body>
 
+        <!--========== PROMO : VIEW ==========-->
     <div class="s-promo-block-v2 g-bg-gradient-darkblue-strong g-fullheight-xs" style="background: url(assets/img/site/mainpagebanner.jpg) no-repeat fixed;">
-        <div class="container  g-padding-y-60-xs">
-            <div class="row g-row-col-0  g-ver-center-xs  g-padding-x-145-sm">
-                <div class="col-sm-12 g-text-center-xs ">
-                        <img class="img-responsive g-margin-auto g-margin-b-35-xs" src="assets/img/site/xremo.png" alt="Dublin Logo">
-                    <h4 class="g-font-size-20-xs text-uppercase g-font-weight-700 g-letter-spacing-2 g-color-white g-margin-b-30-xs">Select country</h4>
+        <div class="container   g-ver-center-xs">
+            <div class="row g-row-col-0  g-padding-x-120-sm g-text-center-xs">
+                <div class="wow fadeInUp animated animation" data-wow-duration=".3" data-wow-delay=".1s">
+                    <img class="img-responsive g-margin-auto g-margin-b-40-xs" src="assets/img/site/xremo.png" alt="Xremo Job Portal">
                 </div>
-                <div class="col-sm-4 col-xs-4 center-block ">
-                    <a href="site/country/my/">
-                        <img src="assets/img/site/malaysia_round_icon_256.png" alt="" class="img-fluid center-block ">
-                    </a>
+                <div class="wow fadeInUp animated animation" data-wow-duration=".3" data-wow-delay=".2s">
+                    <h1 class="g-font-size-34-xs  g-color-md-white-text g-font-weight-600 g-margin-b-20-xs">Discover your Xciting internship and career in Xremo</h1>
                 </div>
-                <div class="col-sm-4  col-xs-4 center-block">
-                    <a href="site/country/ph/">
-                        <img src="assets/img/site/philippines_round_icon_256.png" alt="" class="img-fluid center-block">
-                    </a>
+                <div class="wow fadeInUp animated animation" data-wow-duration=".3" data-wow-delay=".3s">
+                    <div class="g-hor-divider-solid-md-orange g-width-350-sm  center-block g-margin-b-50-xs"></div>
                 </div>
-                <div class="col-sm-4 col-xs-4 center-block">
-                    <a href="site/country/id/">
-                        <img src="assets/img/site/indonesia_round_icon_256.png" alt="" class="img-fluid center-block">
-                    </a>
+            </div>
+
+            <div class="row g-row-col-0  g-padding-x-120-sm g-text-center-xs ">
+                <div class="wow fadeInUp animated animation" data-wow-duration=".3" data-wow-delay=".4s">
+                    <h4 class="font-weight-600 md-white-text">Select your Country</h4>
+                </div>
+                <div class="wow fadeInUp animated animation" data-wow-duration=".3" data-wow-delay=".5s">
+                    <div class="btn-group " role="group" aria-label="...">
+
+                        <a href="site/country/id/" class="btn btn-md-indigo md-white-text btn-lg px-5 mx-2">Indonesia</a>
+                        <a href="site/country/my/" class="btn btn-md-indigo btn-lg mx-2 px-5">Malaysia</a>
+                        <a href="site/country/ph/" class="btn btn-md-indigo btn-lg mx-2 px-5">Philipines</a>
+
+                    </div>
                 </div>
             </div>
         </div>
