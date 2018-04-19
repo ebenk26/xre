b17f7e4bcec651b01362c5986f83fb3bafc64042
amirazain <amirazain2009@gmail.com>
2018-04-19 18:45:56 +0800

    edit footer_app ui

--- application/views/main/footer_app.php
+++ application/views/main/footer_app.php
@@ -3,15 +3,11 @@
         <div class="page-footer-inner"> <?=date('Y')?> ©
 			<a target="_blank" class="mdo-white-v5-text" href="https://www.xremo.com">Xremo Sdn. Bhd.</a> &nbsp;|&nbsp;
 			<a target="_blank" class="mdo-white-v5-text" href="<?=base_url()?>terms-of-use">Terms of Use</a> &nbsp;|&nbsp;
-			<a target="_blank" class="mdo-white-v5-text" href="<?=base_url()?>privacy">Privacy Policy</a><!-- &nbsp;|&nbsp;
-			<a target="_blank" class="mdo-white-v5-text" href="faq.html">FAQ</a>-->
-			<div class="scroll-to-top" style="display: none;">
-				<i class="icon-arrow-up"></i>
+			<a target="_blank" class="mdo-white-v5-text" href="<?=base_url()?>privacy">Privacy Policy</a>
+			<a target="_blank" class="mdo-white-v5-text" href="faq.html">FAQ</a>
+			<div class="scroll-to-top" >
+				<i class="icon-arrow-up md-orange-text"></i>
 			</div>
 		</div>
-		<!--<div class="page-footer-inner"> <?php echo date('Y') ?> © Copyright By Xremo</div>-->
-        <div class="scroll-to-top" style="display: none;">
-            <i class="icon-arrow-up"></i>
-        </div>
     </div>
     <!-- END FOOTER
