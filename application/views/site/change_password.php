0b0bea5585d0d5edc467bbbab45651eab0ca8023
amirazain <amirazain2009@gmail.com>
2018-04-20 18:05:36 +0800

    update change pasword style

--- application/views/site/change_password.php
+++ application/views/site/change_password.php
@@ -8,16 +8,31 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Change Password</title>
 
+    <!-- CSS STYLES -->
     <!-- Web Fonts -->
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:300,400,400i|Montserrat:400,700" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,500,600,700&subset=all">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i|Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,500,600,700&subset=all" rel="stylesheet" type="text/css" />
 
-    <!-- Vendor Styles -->
-    <link rel="stylesheet" type="text/css" href="<?= ASSETS; ?>plugins/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= ASSETS; ?>plugins/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= ASSETS; ?>css/components.css">
+    <!-- Bootstrap -->
+    <link href="<?php echo CSS; ?>bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo CSS; ?>bootstrap/bootstrap-switch.min.css" rel="stylesheet" type="text/css">
 
-    <link rel="stylesheet" type="text/css" href="<?= ASSETS; ?>style.css">
+    <!-- Icon -->
+    <link href="<?php echo CSS; ?>icon/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo CSS; ?>icon/simple-line-icons.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Plugin -->
+    <link href="<?php echo JS; ?>plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo JS; ?>plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Global -->
+    <link href="<?php echo CSS; ?>global/components.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo CSS; ?>global/plugins.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page Layout -->
+    <link href="<?php echo CSS; ?>pages/login-5.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>assets/css/vendor/alertify.min.css" rel="stylesheet" type="text/css">
+    <!-- END PAGE LEVEL STYLES -->
     <!-- Favicon -->
     <link rel="shortcut icon" href="../../custom_pages/favicon.ico" type="image/x-icon">
 </head>
@@ -27,8 +42,7 @@
     <header class="navbar-fixed-top s-header js-header-sticky js-header-overlay">
         <!-- Navbar -->
         <nav class="s-header-v2-navbar">
-            <div class="container g-display-table-lg">
-                <!-- Navbar Row -->
+            <div class="container mt-display-table-lg">                
                 <div class="s-header-v2-navbar-row">
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <div class="s-header-v2-navbar-col">
@@ -41,8 +55,7 @@
                     <div class="s-header-v2-navbar-col ">
                         <div class="s-header-v2-logo">
                             <a href="index.html" class="s-header-v2-logo-link">
-                                <img class="s-header-v2-logo-img s-header-v2-logo-img-default" src="<?= ASSETS; ?>img/xremo/xremo.png" alt="Dublin Logo" style="height:47px">
-                                <!-- <img class="s-header-v2-logo-img s-header-v2-logo-img-default" src="<?= ASSETS; ?>img/xremo/xremo-logo-blue.png" style="height:47px" alt="Dublin Logo"> -->
+                                <img class="s-header-v2-logo-img s-header-v2-logo-img-default height-50" src="<?= IMG; ?>site/xremo-logo-white.svg" alt="Xremo Logo">                                
                             </a>
                         </div>
                     </div>
@@ -59,17 +72,17 @@
     <!--========== END HEADER ==========-->
 
     <!--========== CONTENT ==========-->
-    <section class="s-promo-block-v4 g-bg-gradient-md-indigo g-fullheight-xs">
-        <div class="container g-ver-center-xs pt-4 ">
-            <div class="portlet light px-8 py-5">
+    <section class="s-promo-block-v4 gradient-indigo g-fullheight">
+        <div class="container g-ver-center  ">
+            <div class="portlet light p-100">
                 <div class="portlet-body ">
                     <div class="row">
                         <div class="col-sm-6">
-                            <h2 class="md-indigo-text font-weight-500 primary-font my-4 ">Reset Your Password</h2>
-                            <p class="roboto-font mb-2">You have requested to reset password for:</p>
-                            <p class="font-weight-600">student12@gmail.com</p>
+                            <h2 class="md-indigo-text font-weight-500 my-20 ">Reset Your Password</h2>
+                            <p class="mb-2">You have requested to reset password for:</p>
+                            <h6 class="font-weight-600 text-none">student12@gmail.com</h6>
                         </div>
-                        <div class="col-sm-6 md-grey lighten-4  py-4 px-3">
+                        <div class="col-sm-6 bg-sky-light p-20">
                             <form action="" class="form-horizontal">
                                 <!-- New Password -->
                                 <div class="form-group mx-0 ">
@@ -93,15 +106,55 @@
     </section>
     <!--========== END CONTENT ==========-->
 
+    
 
+    <!-- CORE -->
+    <script type="text/javascript" src="<?php echo JS; ?>plugins/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/jquery-v1-11.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/jquery.migrate.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>bootstrap/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>plugins/js.cookie.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>plugins/jquery.blockui.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
 
-    <!-- Back To Top -->
+    <!-- VENDOR -->
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/jquery.smooth-scroll.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/jquery.back-to-top.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/jquery.equal-height.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/jquery.parallax.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/jquery.wow.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/counterup.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/scrollbar/jquery.scrollbar.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/swiper/swiper.jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/masonry/jquery.masonry.pkgd.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/masonry/imagesloaded.pkgd.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/vendor/cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>plugins/rateit/jquery.rateit.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>plugins/bootstrap-select/js/bootstrap-select.min.js"></script>
 
-    <!-- BEGIN CORE PLUGINS -->
-    <!-- Metronic -->
-    <script type="text/javascript" src="<?= ASSETS; ?>plugins/jquery.min.js"></script>
-    <script type="text/javascript" src="<?= ASSETS; ?>plugins/jquery.migrate.min.js"></script>
-    <script type="text/javascript" src="<?= ASSETS; ?>plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Custom-->
+    <script type="text/javascript" src="<?php echo JS; ?>alertify.min.js"></script>
+
+    <!-- Global-->
+    <script type="text/javascript" src="<?php echo JS; ?>global/app.min.js"></script>
+    <!-- Layout 8 -->
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/layout8.min.js"></script>
+
+    <!-- Component Page -->
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/components/header-sticky.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/components/scrollbar.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/components/magnific-popup.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/components/swiper.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/components/counter.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/components/portfolio-3-col.min.js"></script>
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/components/parallax.min.js"></script>
+    <!-- <script type="text/javascript" src="<?php echo JS; ?>layout8/components/google-map.min.js"></script> -->
+    <script type="text/javascript" src="<?php echo JS; ?>layout8/components/wow.min.js"></script>
+
+    <!-- Page -->
+    <script type="text/javascript" src="<?php echo JS; ?>pages/portfolio-3-gallery.js"></script>
 </body>
 
 </html>
