8fa364ef2f1cd6762b5fd83dbd3caa3a9b7fd002
amirazain <amirazain2009@gmail.com>
2018-05-08 00:19:16 +0800

    edit modal location

--- application/views/employer/job_board.php
+++ application/views/employer/job_board.php
@@ -385,14 +385,13 @@
                     <h4 class="form-section mt-50">Location</h4>
 
                     <div class="row">
-
                         <div class="col-md-6 col-xs-12">
                             <!-- Address -->
                             <div class="form-group mx-0">
                                 <label class="control-label md-grey-darken-3-text mb-10 font-weight-600">Address</label>
                                 <input type="text" name="address" class="form-control" placeholder="Unit / Lot , Road ," value="<?=$user_profile['shipping_address']?>" id="addAddress" required>
                             </div>
-                            <!-- City / State / Postcode-->
+                            <!-- City /  Postcode-->
                             <div class="row ">
                                 <!-- City  -->
                                 <div class="col-md-6">
@@ -412,8 +411,15 @@
 
                             </div>
 
-                            <!-- Country / Latitude / Longititude -->
+                            <!-- state /Country  -->
                             <div class="row">
+                                <!-- State -->
+                                <div class="col-md-6">
+                                    <div class="form-group mx-0 ">
+                                        <label class="control-label md-grey-darken-3-text mb-10 font-weight-600">State</label>
+                                        <input type="text" class="form-control" name="state" value="<?=$user_profile['shipping_state']?>" id="addState" required>
+                                    </div>
+                                </div>
                                 <!-- Country -->
                                 <div class="col-md-6">
                                     <div class="form-group mx-0">
@@ -427,14 +433,9 @@
                                         </select>
                                     </div>
                                 </div>
-                                <!-- State -->
-                                <div class="col-md-6">
-                                    <div class="form-group mx-0 ">
-                                        <label class="control-label md-grey-darken-3-text mb-10 font-weight-600">State</label>
-                                        <input type="text" class="form-control" name="state" value="<?=$user_profile['shipping_state']?>" id="addState" required>
-                                    </div>
-                                </div>
+                                
                             </div>
+                            <!-- Long / Lat -->
                             <div class="row">
                                 <!-- Latitude -->
                                 <div class="col-md-6">
@@ -466,6 +467,7 @@
                             <!-- </div> -->
                         </div>
                     </div>
+
                 </div>
                 <input type="hidden" id="job_status_add" name="status"></input>
                 <div class="modal-footer form-action ">
@@ -481,14 +483,14 @@
 <!-- BEGIN MODAL : EDIT JOB POST -->
 <?php foreach ($job_post as $key => $value) { $location = json_decode($value['location']); ?>
 <div class="modal fade  modal-open " id="modal_edit_jobpost_<?php echo $value['id'] ?>" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog modal-full">
+    <div class="modal-dialog modal-full width-90-percent">
         <div class="modal-content ">
             <!-- TITLE -->
             <div class="modal-header ">
                 <h4 class="modal-title">Edit Job Post </h4>
             </div>
             <!-- FORM -->
-            <form action="<?php echo base_url(); ?>employer/job_board/update/" method="POST" class="form-horizontal  ">
+            <form action="<?php echo base_url(); ?>employer/job_board/update/" method="POST" class="form form-horizontal  ">
                 <input type="hidden" id="job_status_edit" name="status" value="<?php echo !empty($value['status']) ? $value['status']: 'preview'; ?>"></input>
                 <div class="modal-body form-body ">
                     <input type="hidden" name="job_id" value="<?php echo $value['id'] ?>"></input>
@@ -595,79 +597,83 @@
                         <textarea name="additional_Info" data-provide="markdown" rows="10" data-hidden-buttons="cmdCode , cmdQuote" class="textarea_editor"><?php echo $value['additional_info']?></textarea>
                     </div>
 
-                    <h4 class="form-section ">Location</h4>
-                    <!-- Address -->
-                    <div class="row mx-0">
-                        <div class="col-md-12 col-xs-12">
+                    <h4 class="form-section mt-50 ">Location</h4>
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+                            <!-- Address -->
                             <div class="form-group mx-0">
                                 <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">Address</label>
                                 <input type="text" name="address" value="<?php echo !empty($location->address) ? $location->address : ''; ?>" class="form-control" placeholder="Unit / Lot , Road ," id="addAddress<?= $value['id'];?>">
                             </div>
-                        </div>
-                    </div>
+                            <!-- City /  Postcode-->
+                            <div class="row ">
+                                <div class="col-md-6 col-xs-12">
+                                    <div class="form-group mx-0">
+                                        <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">City</label>
+                                        <input type="text" class="form-control" name="city" value="<?php echo !empty($location->city) ? $location->city : ''; ?>" id="addCity<?= $value['id'];?>">
+                                    </div>
+                                </div>    
+                                <div class="col-md-6 col-xs-12">
+                                    <div class="form-group mx-0">
+                                        <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">Postcode</label>
+                                        <input type="text" class="form-control" placeholder="Postcode" name="postcode" value="<?php echo !empty($location->postcode) ? $location->postcode : ''; ?>" id="addPostcode<?= $value['id'];?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- State /Country -->
+                            <div class="row ">
+                                <div class="col-md-6 col-xs-12">
+                                    <div class="form-group mx-0 ">
+                                        <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">State</label>
+                                        <input type="text" class="form-control" name="state" value="<?php echo !empty($location->state) ? $location->state : ''; ?>" id="addState<?= $value['id'];?>">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-xs-12">
+                                    <div class="form-group mx-0">
+                                        <!-- <label class="control-label">Country</label> -->
+                                        <label class="control-label md-grey-darken-3-text mb-10 font-weight-600   ">Country</label>
 
-                    <!-- City / State / Postcode-->
-                    <div class="row mx-0">
-                        <div class="col-md-4 col-xs-12">
-                            <div class="form-group mx-0">
-                                <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">City</label>
-                                <input type="text" class="form-control" name="city" value="<?php echo !empty($location->city) ? $location->city : ''; ?>" id="addCity<?= $value['id'];?>">
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-xs-12">
-                            <div class="form-group mx-0 ">
-                                <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">State</label>
-                                <input type="text" class="form-control" name="state" value="<?php echo !empty($location->state) ? $location->state : ''; ?>" id="addState<?= $value['id'];?>">
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-xs-12">
-                            <div class="form-group mx-0">
-                                <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">Postcode</label>
-                                <input type="text" class="form-control" placeholder="Postcode" name="postcode" value="<?php echo !empty($location->postcode) ? $location->postcode : ''; ?>" id="addPostcode<?= $value['id'];?>">
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Country / Latitude / Longititude-->
-                    <div class="row mx-0">
-                        <div class="col-md-4 col-xs-12">
-                            <div class="form-group mx-0">
-                                <!-- <label class="control-label">Country</label> -->
-                                <label class="control-label md-grey-darken-3-text mb-10 font-weight-600   ">Country</label>
+                                        <select class="form-control" name="country" id="addCountry<?= $value['id'];?>">
+                                            <?php foreach ($countries as $key => $country_value) {?>
+                                            <option <?php echo $location->country == $country_value['name'] ? 'selected' : '' ?>>
+                                                <?php echo $country_value['name']; ?>
+                                            </option>
+                                            <?php } ?>
+                                        </select>
 
-                                <select class="form-control" name="country" id="addCountry<?= $value['id'];?>">
-                                    <?php foreach ($countries as $key => $country_value) {?>
-                                    <option <?php echo $location->country == $country_value['name'] ? 'selected' : '' ?>>
-                                        <?php echo $country_value['name']; ?>
-                                    </option>
-                                    <?php } ?>
-                                </select>
+                                    </div>
+                                </div>
+                            </div> 
+                            <!-- Longititude & Latitude -->
+                            <div class="row">
+                                <div class="col-md-6 col-xs-12">
+                                    <div class="form-group mx-0">
+                                        <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">Latitude</label>
+                                        <input id="latitude<?= $value['id']?>" type="text" class="form-control" placeholder="1.643604 " name="latitude" value="<?php echo !empty($location->latitude) ? $location->latitude : ''; ?>">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-xs-12">
+                                    <div class="form-group mx-0">
+                                        <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">Longititude</label>
+                                        <input id="longitude<?= $value['id']?>" type="text" class="form-control" placeholder="1.955566" name="longitude" value="<?php echo !empty($location->longitude) ? $location->longitude : ''; ?>">
+                                    </div>
+                                </div>
+                            </div>
+                         </div>
+                        <div class="col-md-6 col-sm-12">
+                            <input type="hidden" id="addMapTitle<?= $value['id']?>" name="mapTitle" value="<?= !empty($location->map_title) ? $location->map_title : 'Map Title';?>"></input>
+                            <input type="hidden" id="addMapDescription<?= $value['id']?>" name="mapDescription" value="<?= !empty($location->map_description) ? $location->map_description : 'Map Description';?>"></input>
+                            <div class="row ">
+                                <div class="col-md-12">
+                                    <div class="mb-50 " style="height: 350px; " id="map-window<?= $value['id'];?>">
+                                        <input id="pac-input<?= $value['id'];?>" class="controls" type="text" placeholder="Enter a location">
+                                        <div id="map<?= $value['id'];?>" style="height: 100%; z-index: 9999 !important"></div>
 
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-md-4 col-xs-12">
-                            <div class="form-group mx-0">
-                                <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">Latitude</label>
-                                <input id="latitude<?= $value['id']?>" type="text" class="form-control" placeholder="1.643604 " name="latitude" value="<?php echo !empty($location->latitude) ? $location->latitude : ''; ?>">
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-xs-12">
-                            <div class="form-group mx-0">
-                                <label class="control-label md-grey-darken-3-text mb-10 font-weight-600  ">Longititude</label>
-                                <input id="longitude<?= $value['id']?>" type="text" class="form-control" placeholder="1.955566" name="longitude" value="<?php echo !empty($location->longitude) ? $location->longitude : ''; ?>">
-                            </div>
-                        </div>
-                    </div>
-                    <input type="hidden" id="addMapTitle<?= $value['id']?>" name="mapTitle" value="<?= !empty($location->map_title) ? $location->map_title : 'Map Title';?>"></input>
-                    <input type="hidden" id="addMapDescription<?= $value['id']?>" name="mapDescription" value="<?= !empty($location->map_description) ? $location->map_description : 'Map Description';?>"></input>
-                    <div class="row ">
-                        <div class="col-md-12">
-                            <div class="mb-50 " style="height: 400px; " id="map-window<?= $value['id'];?>">
-                                <input id="pac-input<?= $value['id'];?>" class="controls" type="text" placeholder="Enter a location">
-                                <div id="map<?= $value['id'];?>" style="height: 100%; z-index: 9999 !important"></div>
-
-                            </div>
-                        </div>
-                    </div>
+                    </div>                    
                 </div>
                 <div class="modal-footer form-action ">
                     <a data-dismiss="modal" aria-hidden="true" class="btn btn-outline btn-md-indigo  letter-space-xs">Cancel</a>
