80c6ddce8515cb81df43cecb77a37c0b9370cc55
amirazain <amirazain2009@gmail.com>
2018-06-28 18:17:56 +0800

    edit view profile mobile version and empty states

--- application/views/student/main/profile_missing.php
+++ application/views/student/main/profile_missing.php
@@ -1,19 +1,17 @@
 <?php if(empty($roles) || $this->session->userdata('id') != $user_profile['overview']['id_users']){ ?>
-    <div class="portlet p-50 md-grey-lighten-5">
-        <div class="portlet-body text-center">
-            <i class="icon-puzzle font-grey-mint font-40 mb-25"></i>
-            <h5 class="text-center font-weight-500 font-grey-mint">Found Nothing</h5>
-            <h6 class="text-center font-16 font-grey-cascade mt-10 font-weight-400 text-none mb-1">User haven't update profile info. </h6>
+    <div class="portlet py-30 px-20 md-grey-light ">
+        <div class="portlet-body text-center">            
+            <h5 class="text-center font-weight-500 font-grey-mint font-18 font-20-sm ">No Info!</h5>
+            <p class="text-center  font-grey-cascade mt-10 font-weight-400 text-none mb-1 font-14">User haven't update profile info. </p>
         </div>
     </div>
 <?php }?>
 <?php if(!empty($roles) && $this->session->userdata('id') == $user_profile['overview']['id_users']){ ?>
-    <div class="portlet p-50 md-grey-lighten-5">
-        <div class="portlet-body text-center">
-            <i class="icon-ghost font-grey-mint font-40 mb-25"></i>
-            <h5 class="text-center font-weight-500 font-grey-mint text-none">This section is empty</h5>
-            <h6 class="text-center  font-grey-cascade font-16 mt-10 mb-15 text-none">Add info here to make your resume look great.</h6>
-            <a href="<?=base_url()?>student/profile" class="btn btn-outline btn-md-indigo rounded-4"> Edit My Profile</a>
+    <div class="portlet py-30 px-20 md-grey-light">
+        <div class="portlet-body text-center">            
+            <h5 class="text-center font-weight-600 font-grey-mint text-none font-18 font-20-sm ">No Info!</h5>
+            <p class="text-center  font-grey-cascade font-400 font-14 mt-10 mb-15 text-none">Edit your info in here to make your resume look great.</p>
+            <a href="<?=base_url()?>student/profile" class="btn  btn-md-indigo letter-space-sm text-uppercase font-13 font-weight-600"> <i class="icon-pencil mr-5"></i> Edit my profile</a>
         </div>
     </div>
 <?php }?>
