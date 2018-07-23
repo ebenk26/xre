a3293c608211af59ab80d9409851d1d5403af60b
Dea Rico Khasandra <rico.k@elabram.com>
2018-07-23 10:44:23 +0700

    update job board text editor

--- application/views/employer/main/footer.php
+++ application/views/employer/main/footer.php
@@ -102,6 +102,8 @@ $company_address = json_decode($user_profile['address']);?>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/alertify.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/pass-strength.js"></script>
 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/plugins/tinymce/tinymce.min.js"></script>
+
 <!--SCRIPTS -->
 <?php if ($this->uri->segment(2) == 'job_board') :?>
 <!--Job Post-->
@@ -765,6 +767,25 @@ $company_address = json_decode($user_profile['address']);?>
 
 <script>
     $(document).ready(function () {
+
+        tinymce.init({
+            selector: '.textarea_editor',
+            height: 400,
+            theme: 'modern',
+            plugins: 'code advlist autolink lists link image charmap hr anchor pagebreak searchreplace wordcount visualblocks visualchars fullscreen insertdatetime nonbreaking save table contextmenu directionality template paste textcolor colorpicker textpattern imagetools codesample toc',
+            toolbar1: 'code undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            toolbar2: 'fontsizeselect forecolor backcolor emoticons | codesample',
+            fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
+            image_advtab: true,
+            templates: [
+                { title: 'Test template 1', content: 'Test 1' },
+                { title: 'Test template 2', content: 'Test 2' }
+            ],
+            content_css: [
+                '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                '//www.tinymce.com/css/codepen.min.css'
+            ]
+        });
 
         var e = $("#xremo_table");
         e.dataTable({
