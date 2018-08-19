527aabdc44c254a66c4716fb23673e840a0c5fed
Dea Rico Khasandra <rico.k@elabram.com>
2018-08-19 19:31:20 +0700

    update image header check

--- application/views/jobseeker/main/header.php
+++ application/views/jobseeker/main/header.php
@@ -172,13 +172,14 @@
                         <!-- END NOTIFICATION DROPDOWN -->
                         <?php
                         !empty($user_profile['profile_photo']) ? $user_profile['profile_photo'] = $user_profile['profile_photo'] : $user_profile['profile_photo'] = 'profile-pic.png'; 
-                        $checkGetProfileImage = get_headers(IMG_STUDENTS.$user_profile['profile_photo']);
+                        $checkGetProfileImage = file_exists(IMG_STUDENTS.$user_profile['profile_photo']);
                         $roles = $this->session->userdata('roles'); ?>
                         <!-- BEGIN USER LOGIN DROPDOWN -->
                         <!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
                         <li class="dropdown dropdown-user">
                             <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                                <img alt="" class="avatar avatar-xtramini avatar-circle" src="<?= $checkGetProfileImage[0] == 'HTTP/1.1 200 OK' ? IMG_STUDENTS.$user_profile['profile_photo'] : IMG_STUDENTS.'profile-pic.png'; ?>" />
+                                <img alt="" class="avatar avatar-xtramini avatar-circle" src="<?= //$checkGetProfileImage[0] == 'HTTP/1.1 200 OK' ? IMG_STUDENTS.$user_profile['profile_photo'] : IMG_STUDENTS.'profile-pic.png';
+                                $checkGetProfileImage == true ? IMG_STUDENTS.$user_profile['profile_photo'] : IMG_STUDENTS.'profile-pic.png';?>" />
                                 <span class="username username-hide-on-mobile">
                                     <?php
                                         $fullname = $this->session->userdata('name');
